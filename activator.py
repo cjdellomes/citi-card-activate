2a72569f37ffcaea5fb61f326849ecd794f722a6
Christopher Dellomes <cjdellomes@gmail.com>
2020-08-28 20:39:19 -0700

    Connected pin permutation generator and post request generator.

--- activator.py
+++ activator.py
@@ -1,25 +1,27 @@
 import requests
 import config
-import json
+from permute import permute
 
 CONTINUE_URL = 'https://www.cardactivation.citi.com/Home/Continue1'
-CARD_NUMBER = config.card_number
-SECURITY_CODE = config.security_code
+POSSIBLE_PIN_DIGITS = [1, 3, 4, 5, 9]
+PIN_DIGIT_COUNT = 4
 
+card_number = config.card_number
+security_code = config.security_code
 
-def continue_request(url, card_number, security_code, pin):
-    continue_data = {
+possible_pins = permute(POSSIBLE_PIN_DIGITS, PIN_DIGIT_COUNT)
+
+for pin in possible_pins:
+    request_payload = {
         'cardNumber': card_number,
         'securityCode': security_code,
         'ssn': pin
     }
 
-    r = requests.post(url, json=continue_data)
+    continue_response = requests.post(CONTINUE_URL, json=request_payload)
+    continue_response_payload = continue_response.json()
+    status_code = continue_response_payload['Status']
 
-    return r
-
-
-continue_response = continue_request(
-    CONTINUE_URL, CARD_NUMBER, SECURITY_CODE, 1433).json()
-
-print(continue_response)
+    if status_code != '999':
+        print(pin)
+        break
